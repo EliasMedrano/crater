0e116a51880ab8cc5b948f98d6be95b6a8ccdbd5
Elías Medrano <140011467+EliasMedrano@users.noreply.github.com>
2024-04-16 09:32:15 -0600

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(CurrenciesTableSeeder::class);
-        $this->call(CountriesTableSeeder::class);
+       /* $this->call(CountriesTableSeeder::class);*/
         $this->call(UsersTableSeeder::class);
     }
 }
